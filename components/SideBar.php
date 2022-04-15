2f4f6832501bd8b6259c7f6ed4b5fac785b47670
Cairo <cairofelipedev@gmail.com>
2022-04-15 18:26:28 -0300

    Add components

---
+++ components/SideBar.php
@@ -0,0 +1,44 @@
+
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+      <li class="nav-item">
+        <a class="nav-link text-black border" href="index.html">
+          <i class="bi biblack text-white"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-black border" href="index.html">
+          <i class="text-black bi bi-layout-text-window-reverse"></i>
+          <span>Posts</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-black border" href="index.html">
+          <i class="text-black bi bi-bar-chart"></i>
+          <span>Ranking</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-black border" href="index.html">
+          <i class="text-black bi bi-person"></i>
+          <span>Usuários</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-black border" href="index.html">
+          <i class="text-black bi bi-journal-text"></i>
+          <span>Material</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-black" href="index.html">
+          <i class="text-black bi bi-box-arrow-in-right"></i>
+          <span>Sair</span>
+        </a>
+      </li>
+    </ul>
+  </aside><!-- End Sidebar-->
